a5221dd8e2d5df6e98a5816b7d263ab545a782c0
Gabriel Theodoropoulos <gabriel@gabriels-mbp.lan>
2016-03-28 21:36:55 +0300

    Initial Commit

---
+++ QuickLookDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QuickLookDemo
+//
+//  Created by Gabriel Theodoropoulos on 3/28/16.
+//  Copyright © 2016 Appcoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
